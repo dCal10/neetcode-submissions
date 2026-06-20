cdba78942709ad29844c6592898d55d3b3e0aea0
Arya Chhabra <78664310+dCal10@users.noreply.github.com>
2026-06-20 11:50:00 -0400

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findLongestLength(self, everyNum : List[int]) -> int:
+        currLen = 0
+        maxLen = 0
+        for num in everyNum:
+            if num > 0:
+                currLen += 1
+            else:
+                maxLen = max(maxLen, currLen)
+                currLen = 0
+        maxLen = max(maxLen, currLen)
+        return maxLen
+                
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0: return 0
+        minNum = min(nums)
+        maxNum = max(nums)
+        everyNum = [0] * (maxNum - minNum + 1) # total of max - min elements
+        for num in nums:
+            everyNum[num - minNum] += 1
+        
+        return self.findLongestLength(everyNum)
+        
