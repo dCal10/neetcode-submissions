6dcbfb40da767afa7f249625b6a3e21713dafc63
Arya Chhabra <78664310+dCal10@users.noreply.github.com>
2026-06-21 14:27:09 -0400

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        every = []
+        for index, pos in enumerate(position):
+            every.append([pos, speed[index]])
+        every.sort(reverse = True)
+
+        cycles = []
+        for pos, speed in every:
+            cycles.append((target - pos) / speed)
+        
+        fleets = 0
+        curr = cycles[0]
+        n = len(cycles)
+        i = 0
+        while i < n:
+            while i < n and cycles[i] <= curr:
+                i += 1
+            fleets += 1
+            if i < n: curr = cycles[i]
+        return fleets
+
